3bf428775a84112f1f515a2390701c29829dac0d
Soumik Hasan <soumik.hasan.smk@gmail.com>
2025-04-01 21:38:28 +0600

    April Fools Day Contest 2025

---
+++ April Fools Day Contest 2025/A_Piecing_it_Together.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define db double
+int main()
+{
+    cout << "puzzling" << endl;
+    return 0;
+}
---
+++ April Fools Day Contest 2025/B_Plinko.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define db double
+int main()
+{
+    string s;
+    getline(cin, s);
+    cout << 1 << endl;
+    return 0;
+}
